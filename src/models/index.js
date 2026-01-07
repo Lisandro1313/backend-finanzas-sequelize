06ca6d564f6a42134fab8563c7f6ab2989f1fd62
Lisandro Etcheverry <lisandro@example.com>
2026-01-06 21:30:36 -0300

    fix: Handle DATABASE_URL connection in production

--- src/models/index.js
+++ src/models/index.js
@@ -4,17 +4,23 @@ const config = require('../config/database');
 const env = process.env.NODE_ENV || 'development';
 const dbConfig = config[env];
 
-const sequelize = new Sequelize(
-    dbConfig.database,
-    dbConfig.username,
-    dbConfig.password,
-    {
-        host: dbConfig.host,
-        port: dbConfig.port,
-        dialect: dbConfig.dialect,
-        logging: false
-    }
-);
+let sequelize;
+
+if (dbConfig.use_env_variable) {
+    sequelize = new Sequelize(process.env[dbConfig.use_env_variable], dbConfig);
+} else {
+    sequelize = new Sequelize(
+        dbConfig.database,
+        dbConfig.username,
+        dbConfig.password,
+        {
+            host: dbConfig.host,
+            port: dbConfig.port,
+            dialect: dbConfig.dialect,
+            logging: false
+        }
+    );
+}
 
 const db = {
     sequelize,
